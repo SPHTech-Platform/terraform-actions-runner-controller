8369e919220ec6bbbed9f692b0ed6eeefe475b29
niroz89 <niro@sph.com.sg>
2022-07-22 06:14:20 +0800

    Update provider

--- versions.tf
+++ versions.tf
@@ -5,9 +5,9 @@ terraform {
       source  = "hashicorp/helm"
       version = ">= 2.6"
     }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = ">= 2.12"
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.14"
     }
   }
 }
