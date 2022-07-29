802a642098783b38ddd8e00aad7ab3bf76ec0fe1
niroz89 <niro@sph.com.sg>
2022-07-30 06:22:43 +0800

    Print out org runners

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "helm_release" {
   description = "Output of the helm release"
   value       = helm_release.release
 }
+
+output "org_runners" {
+  description = "Output of Github Org Runners"
+  value       = kubernetes_manifest.github_org_runners
+}
