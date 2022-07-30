6e208e1046e22cb12ef5a62369d0cc3efba9986f
niroz89 <niro@sph.com.sg>
2022-07-30 08:14:18 +0800

    Switch to kubernetes_manifest

--- org_runners.tf
+++ org_runners.tf
@@ -1,31 +1,3 @@
-# resource "kubectl_manifest" "github_org_runners" {
-#   for_each  = { for org in var.github_org_runners : org.name => org }
-#   yaml_body = <<YAML
-# apiVersion: actions.summerwind.dev/v1alpha1
-# kind: RunnerDeployment
-# metadata:
-#   name: ${lower(each.value.name)}-runner-deployment
-#   namespace: ${var.chart_namespace}
-# spec:
-#   replicas: ${each.value.replicas}
-#   template:
-#     spec:
-#       organization: ${each.value.name}
-#       serviceAccountName: ${var.service_account_name}
-#       group: %{if each.value.group != null}${each.value.group}%{else}"Default"%{endif}
-#       imagePullPolicy: IfNotPresent
-#       securityContext:
-#         fsGroup: 1000
-#       labels:
-#         - ${each.value.label}
-#       resources: %{if each.value.resources != null}${jsonencode(each.value.resources)}%{else}{}%{endif}
-#       tolerations: %{if each.value.tolerations != null}${jsonencode(each.value.tolerations)}%{else}[]%{endif}
-#       affinity: %{if each.value.affinity != null}${jsonencode(each.value.affinity)}%{else}{}%{endif}
-# YAML
-
-#   depends_on = [helm_release.release]
-# }
-
 resource "kubernetes_manifest" "github_org_runners" {
   for_each   = { for org in var.github_org_runners : org.name => org }
   depends_on = [helm_release.release]
--- versions.tf
+++ versions.tf
@@ -5,9 +5,9 @@ terraform {
       source  = "hashicorp/helm"
       version = ">= 2.6"
     }
-    kubectl = {
-      source  = "gavinbunney/kubectl"
-      version = ">= 1.14"
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.12"
     }
   }
   experiments = [module_variable_optional_attrs]
