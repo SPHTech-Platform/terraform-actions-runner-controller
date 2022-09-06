47a7f6a7175b2d5472b0be1d3cda5eb6ecd79902
Uchinda Padmaperuma <uchinda@sph.com.sg>
2022-09-06 17:30:26 +0800

    change the kubernetes_manifest resource

--- org_runners.tf
+++ org_runners.tf
@@ -1,33 +1,67 @@
-resource "kubernetes_manifest" "github_org_runners" {
+# resource "kubernetes_manifest" "github_org_runners" {
+#   for_each   = { for org in var.github_org_runners : org.name => org }
+#   depends_on = [helm_release.release]
+
+#   manifest = {
+#     apiVersion = "actions.summerwind.dev/v1alpha1"
+#     kind       = "RunnerDeployment"
+
+#     metadata = {
+#       name      = "${lower(each.value.name)}-runner-deployment"
+#       namespace = var.chart_namespace
+#     }
+
+#     spec = {
+#       replicas = each.value.replicas
+#       template = {
+#         spec = {
+#           organization       = each.value.name
+#           serviceAccountName = var.service_account_name
+#           group              = each.value.group
+#           imagePullPolicy    = "IfNotPresent"
+#           securityContext = {
+#             fsGroup = 1000
+#           }
+#           labels      = [each.value.label]
+#           resources   = each.value.resources
+#           tolerations = each.value.tolerations
+#           affinity    = each.value.affinity
+#         }
+#       }
+#     }
+#   }
+# }
+
+resource "helm_release" "github_org_runners" {
   for_each   = { for org in var.github_org_runners : org.name => org }
   depends_on = [helm_release.release]
 
-  manifest = {
-    apiVersion = "actions.summerwind.dev/v1alpha1"
-    kind       = "RunnerDeployment"
+  name       = "external-secrets-cluster-store"
+  repository = "https://charts.itscontained.io"
+  chart      = "raw"
+  version    = "0.2.5"
 
-    metadata = {
-      name      = "${lower(each.value.name)}-runner-deployment"
-      namespace = var.chart_namespace
-    }
-
-    spec = {
-      replicas = each.value.replicas
-      template = {
-        spec = {
-          organization       = each.value.name
-          serviceAccountName = var.service_account_name
-          group              = each.value.group
-          imagePullPolicy    = "IfNotPresent"
-          securityContext = {
-            fsGroup = 1000
-          }
-          labels      = [each.value.label]
-          resources   = each.value.resources
-          tolerations = each.value.tolerations
-          affinity    = each.value.affinity
-        }
-      }
-    }
-  }
+  values = [
+    <<-EOF
+    apiVersion: actions.summerwind.dev/v1alpha1
+    kind: RunnerDeployment
+    metadata:
+      name: "${lower(each.value.name)}-runner-deployment"
+      namespace: "${var.chart_namespace}"
+    spec:
+      replicas: "${each.value.replicas}"
+      template:
+        spec:
+          organization: "${each.value.name}"
+          serviceAccountName: "${var.service_account_name}"
+          group: "${each.value.group}"
+          imagePullPolicy: IfNotPresent
+          securityContext:
+            fsGroup: 1000
+          labels: "${each.value.label}"
+          resources: "${each.value.resources}"
+          tolerations: "${each.value.tolerations}"
+          affinity: "${each.value.affinity}"
+    EOF
+  ]
 }
