15b5486df25aeb5c0fb54bdefb05557094b25c93
Poh Peng Ric Wang <7760361+thepoppingone@users.noreply.github.com>
2022-12-08 10:48:22 +0800

    Add autoscaling to github runners (#7)
    add autoscaling to github runners
    
    Co-authored-by: Poh Peng <thepoppingone@users.noreply.github.com>

--- org_runners.tf
+++ org_runners.tf
@@ -11,7 +11,6 @@ resource "kubernetes_manifest" "github_org_runners" {
     }
 
     spec = {
-      replicas = each.value.replicas
       template = {
         spec = {
           organization       = each.value.name
@@ -27,6 +26,37 @@ resource "kubernetes_manifest" "github_org_runners" {
           affinity    = each.value.affinity
         }
       }
+    }
+  }
+}
+
+resource "kubernetes_manifest" "github_org_runners_horizontal_autoscaler" {
+  for_each = { for org in var.github_org_runners : org.name => org }
+
+  manifest = {
+    apiVersion = "actions.summerwind.dev/v1alpha1"
+    kind       = "HorizontalRunnerAutoscaler"
+
+    metadata = {
+      name      = "${lower(each.value.name)}-runner-deployment"
+      namespace = helm_release.release.namespace
+    }
+
+    spec = {
+      minReplicas = each.value.min_replicas
+      maxReplicas = each.value.max_replicas
+      scaleTargetRef = {
+        kind = "RunnerDeployment"
+        name = "${lower(each.value.name)}-runner-deployment"
+      }
+      scaleUpTriggers = [
+        {
+          githubEvent = {
+            workflowJob = {}
+          }
+          duration = each.value.scale_up_trigger_duration
+        },
+      ]
     }
   }
 }
