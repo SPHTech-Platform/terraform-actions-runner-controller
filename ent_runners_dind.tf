5082a665cbc1d91713d3ba1d456a86edaf703b97
Poh Peng Ric Wang <7760361+thepoppingone@users.noreply.github.com>
2023-04-21 17:33:46 +0800

    Add dind runners for fargate workloads (#20)
    Co-authored-by: Poh Peng <thepoppingone@users.noreply.github.com>

---
+++ ent_runners_dind.tf
@@ -0,0 +1,102 @@
+resource "kubernetes_manifest" "github_ent_runners_dind" {
+  for_each = { for ent in var.github_ent_runners_dind : ent.label => ent }
+
+  manifest = {
+    apiVersion = "actions.summerwind.dev/v1alpha1"
+    kind       = "RunnerDeployment"
+
+    metadata = {
+      name      = "${lower(each.value.label)}-runner-deployment"
+      namespace = helm_release.release.namespace
+    }
+
+    spec = {
+      template = {
+        spec = {
+          enterprise                   = each.value.name
+          dockerdWithinRunnerContainer = true
+          image                        = "summerwind/actions-runner-dind-rootless"
+          serviceAccountName           = var.service_account_name
+          group                        = each.value.group
+          imagePullPolicy              = "IfNotPresent"
+          securityContext = {
+            fsGroup = 1000
+          }
+          labels      = [each.value.label]
+          resources   = each.value.resources
+          tolerations = each.value.tolerations
+          affinity    = each.value.affinity
+          volumeMounts = [
+            {
+              mountPath = "/home/runner/.docker/config.json"
+              subPath   = "config.json"
+              name      = "docker-secret"
+            },
+            {
+              mountPath = "/home/runner/.docker"
+              name      = "docker-config-volume"
+            },
+          ]
+          volumes = [
+            {
+              name = "docker-secret"
+              secret = {
+                secretName = "regcred"
+                items = [
+                  {
+                    key  = ".dockerconfigjson"
+                    path = "config.json"
+                  }
+                ]
+              }
+            },
+            {
+              name     = "docker-config-volume"
+              emptyDir = {}
+            }
+          ]
+
+        }
+      }
+    }
+  }
+
+  depends_on = [
+    helm_release.release
+  ]
+}
+
+resource "kubernetes_manifest" "github_ent_runners_horizontal_autoscaler_dind" {
+  for_each = { for ent in var.github_ent_runners_dind : ent.label => ent }
+
+  manifest = {
+    apiVersion = "actions.summerwind.dev/v1alpha1"
+    kind       = "HorizontalRunnerAutoscaler"
+
+    metadata = {
+      name      = "${lower(each.value.label)}-runner-deployment"
+      namespace = helm_release.release.namespace
+    }
+
+    spec = {
+      minReplicas = each.value.min_replicas
+      maxReplicas = each.value.max_replicas
+      scaleTargetRef = {
+        kind = "RunnerDeployment"
+        name = "${lower(each.value.label)}-runner-deployment"
+      }
+      scaleUpTriggers = [
+        {
+          githubEvent = {
+            workflowJob = {}
+          }
+          duration = each.value.scale_up_trigger_duration
+        },
+      ]
+    }
+  }
+
+  depends_on = [
+    helm_release.release
+  ]
+}
