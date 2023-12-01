415818dce5698b1b2725dffe1b4c63be6751f602
Poh Peng <thepoppingone@users.noreply.github.com>
2023-12-01 13:54:31 +0800

    Set correct default chart version

--- modules/gha-runner-scale-set/variables.tf
+++ modules/gha-runner-scale-set/variables.tf
@@ -22,7 +22,7 @@ variable "chart_repository" {
 variable "chart_version" {
   description = "Version of Chart to install. Set to empty to install the latest version."
   type        = string
-  default     = "0.6.0"
+  default     = "0.6.1"
 }
 
 variable "chart_namespace" {
